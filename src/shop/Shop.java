20a8e540534220750bff0a89c6ad232d98ccecb0
MilanaND6 <90847879+MilanaND6@users.noreply.github.com>
2021-11-05 11:06:49 -0400

    Thingy

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package shop;
+
 import java.util.*;
+
 /**
  *
  * @author SPH_SHSM
@@ -15,14 +17,19 @@ public class Shop {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayList<Item> stock=new ArrayList<>();
-        stock.add(new Item("Computer Monitor",159.99,5));
-        stock.add(new Item("Keyboard",29.95,100));
-        stock.add(new Item("Mouse",15.00,0));
-        
-        for (Item i:stock){
+        System.out.println("Welcome to Milana's clothing shop!\n");
+        ArrayList<Item> stock = new ArrayList<>();
+        stock.add(new Item("Butterfly Print Crop Tee \n", 10.99, 43));
+        stock.add(new Item("Contrast Stitch Bustier Tee \n", 12.99, 27));
+        stock.add(new Item("Skeleton Graphic Tee \n", 14.99, 0));
+        stock.add(new Item("Graphic Print Wide Leg Pants \n", 34.99, 4));
+        stock.add(new Item("Pleated Varisty Skirt \n", 9.99, 5));
+        stock.add(new Item("Shirred Puff Sleeve Dress \n", 15.99, 10));
+        stock.add(new Item("1pc Off Shoulder Lace Dress \n", 29.99, 0));
+
+        for (Item i : stock) {
             i.display();
         }
     }
-    
+
 }
