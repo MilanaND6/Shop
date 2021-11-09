634285d91bb54c5ea34c415dfc8a426aca3696f5
MilanaND6 <90847879+MilanaND6@users.noreply.github.com>
2021-11-09 11:08:50 -0500

    not finito

--- src/shop/Shop.java
+++ src/shop/Shop.java
@@ -17,19 +17,25 @@ public class Shop {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Milana's clothing shop!\n");
         ArrayList<Item> stock = new ArrayList<>();
-        stock.add(new Item("Butterfly Print Crop Tee \n", 10.99, 43));
-        stock.add(new Item("Contrast Stitch Bustier Tee \n", 12.99, 27));
-        stock.add(new Item("Skeleton Graphic Tee \n", 14.99, 0));
-        stock.add(new Item("Graphic Print Wide Leg Pants \n", 34.99, 4));
-        stock.add(new Item("Pleated Varisty Skirt \n", 9.99, 5));
-        stock.add(new Item("Shirred Puff Sleeve Dress \n", 15.99, 10));
-        stock.add(new Item("1pc Off Shoulder Lace Dress \n", 29.99, 0));
+        stock.add(new Item("(1) Butterfly Print Crop Tee \n", 10.99, 43));
+        stock.add(new Item("(2) Contrast Stitch Bustier Tee \n", 12.99, 27));
+        stock.add(new Item("(3) Skeleton Graphic Tee \n", 14.99, 0));
+        stock.add(new Item("(4) Graphic Print Wide Leg Pants \n", 34.99, 4));
+        stock.add(new Item("(5) Pleated Varisty Skirt \n", 9.99, 5));
+        stock.add(new Item("(6) Shirred Puff Sleeve Dress \n", 15.99, 10));
+        stock.add(new Item("(7) 1pc Off Shoulder Lace Dress \n", 29.99, 0));
 
         for (Item i : stock) {
             i.display();
         }
+        System.out.println("\nWhat item would you like to choose? Please pick from item number 1-7.");
+        String options = in.nextLine(); 
     }
-
+    
+    public void cart() {
+        
+    }
 }
